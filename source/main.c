f9f72970b64d0f0a78bad17d7e86170d01a88461
zzz107360224 <54355fgh@gmail.com>
2018-09-26 22:10:02 +0800

    Complete Lab1

---
+++ source/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+		printf("Welcome to C!\n");
+		printf("My student id is :107360224\n");
+		printf("My name is ³¯«Ø¦ë\n");
+
+		system("pause");
+		return 0;
+}
